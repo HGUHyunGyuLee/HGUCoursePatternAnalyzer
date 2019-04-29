c446334dd7ef433aede290f5a4501b9a795043a7
lhyun <lhyun@192.168.0.12>
2019-04-29 23:55:56 +0900

    This is commit for HW4

---
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -0,0 +1,16 @@
+package edu.handong.analysis.datamodel;
+
+public class Course {
+	private String courseName;
+	
+	
+	public Course(String name) {
+		courseName = name;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String name) {
+		courseName = name;
+	}
+}
